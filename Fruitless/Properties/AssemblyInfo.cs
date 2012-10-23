521b0eb62a2cebdb3eafcb6a578256255f2f41c4
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2012-10-24 00:50:28 +0200

    Fixed up name properties.

--- Fruitless/Properties/AssemblyInfo.cs
+++ Fruitless/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Fruitless")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A 2D-focused game engine.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Fruitless")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCompany("Jacob Hauberg Hansen")]
+[assembly: AssemblyProduct("Fruitless Games Library")]
+[assembly: AssemblyCopyright("Copyright 2012 Jacob Hauberg Hansen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,6 +19,8 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("896923a2-7226-4bf9-8d12-e3c967e6f1f7")]
@@ -34,3 +37,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
