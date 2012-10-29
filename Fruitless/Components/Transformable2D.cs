ca10772e25fb14dbc5eb9ba0fe9b9d98b06a8dd0
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2012-10-29 23:48:11 +0100

    Position now returns the transformed and final position. RelativePosition has been added for retrieving the position relative to its parent.

--- Fruitless/Components/Transformable2D.cs
+++ Fruitless/Components/Transformable2D.cs
@@ -1,9 +1,12 @@
 ﻿using OpenTK;
 
 namespace Fruitless.Components {
-    public class Transformable2D : TransformationComponent {
-        private float _rotationInRadians = 0;
+    public class Transformable2D : TransformationComponent { 
+        float _rotationInRadians = 0;
 
+        /// <summary>
+        /// The angle of rotation in radians.
+        /// </summary>
         public float Rotation {
             get {
                 return _rotationInRadians;
@@ -17,8 +20,11 @@ namespace Fruitless.Components {
             }
         }
 
-        private Vector2 _scale = Vector2.One;
+        Vector2 _scale = Vector2.One;
 
+        /// <summary>
+        /// Defaults to Vector2.One (1, 1).
+        /// </summary>
         public Vector2 Scale {
             get {
                 return _scale;
@@ -32,9 +38,12 @@ namespace Fruitless.Components {
             }
         }
 
-        private Vector2 _position = Vector2.Zero;
+        Vector2 _position = Vector2.Zero;
 
-        public Vector2 Position {
+        /// <summary>
+        /// The position in pixels relative to its parent.
+        /// </summary>
+        public Vector2 RelativePosition {
             get {
                 return _position;
             }
@@ -46,13 +55,22 @@ namespace Fruitless.Components {
                 }
             }
         }
-
+        
+        /// <summary>
+        /// The absolute position in pixels.
+        /// </summary>
+        public Vector2 Position {
+            get {
+                return new Vector2(World.M41, World.M42);
+            }
+        }
+        
         public override Matrix4 Local {
             get {
                 return
                     Matrix4.Scale(Scale.X, Scale.Y, 1) *
                     Matrix4.CreateRotationZ(_rotationInRadians) *
-                    Matrix4.CreateTranslation(Position.X, Position.Y, 0);
+                    Matrix4.CreateTranslation(RelativePosition.X, RelativePosition.Y, 0);
             }
         }
     }
