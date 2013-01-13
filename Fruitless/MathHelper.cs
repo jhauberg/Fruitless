36a1d524e9d662467c00834580a149a8ffe1d561
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2013-01-13 13:55:28 +0100

    Added float utility method.

--- Fruitless/MathHelper.cs
+++ Fruitless/MathHelper.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using OpenTK;
+using System;
 
 namespace Fruitless {
     public static class MathHelper {
         public const float Pi = (float)Math.PI;
         public const float HalfPi = (float)(Math.PI / 2);
 
-        public static float Lerp(double from, double to, double step) {
-            return (float)((to - from) * step + from);
+        public static float Lerp(float from, float to, float step) {
+            return (to - from) * step + from;
+        }
+
+        public static float SmoothStep(float min, float max, float step) {
+            float r = (step - min) / (max - min);
+
+            return r * r * (3.0f - 2.0f * r);  
+        }
+    }
+
+    public static class FloatExtensions {
+        public const Single Precision = 0.00001f;
+
+        public static bool IsAlmostEqualTo(this Single value, Single other) {
+            return (Single)Math.Abs(other - value) < Precision;
         }
     }
 }
