673b9fe0d205f76a18c2eab159a1259e3782ce79
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2013-01-13 11:58:18 +0100

    Added slightly modified version of Nick Gravelyn's Unity TaskManager (https://gist.github.com/2480060). It's so useful!

---
+++ Fruitless/Components/TaskManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fruitless.Components {
+    public class TaskManager : TimelineComponent {
+        public static TaskManager Main {
+            get;
+            set;
+        }
+
+        // cache of queues we can reuse to avoid garbage
+        readonly Stack<TaskQueue> queueCache = new Stack<TaskQueue>();
+
+        // active queues
+        readonly List<TaskQueue> queues = new List<TaskQueue>();
+
+        public TaskManager() {
+            if (Main == null) {
+                Main = this;
+            }
+        }
+
+        public override void Advance(TimeSpan delta) {
+            // update all queues (backwards since we will be modifying the list)
+            for (int i = queues.Count - 1; i >= 0; i--) {
+                queues[i].Advance(delta);
+
+                // if the queue is complete put it in the cache and remove it from the list
+                if (queues[i].IsComplete) {
+                    queueCache.Push(queues[i]);
+                    queues.RemoveAt(i);
+                }
+            }
+        }
+
+        public void StopAllTaskQueues() {
+            // stop each queue and put it into the cache
+            foreach (var q in queues) {
+                q.Stop();
+                queueCache.Push(q);
+            }
+
+            // clear our active list
+            queues.Clear();
+        }
+
+        public TaskQueue WaitFor(float seconds) {
+            // find a cached queue or make a new one
+            TaskQueue queue = queueCache.Count > 0 ? queueCache.Pop() : new TaskQueue();
+
+            // store the queue in our active list
+            queues.Add(queue);
+
+            // queue up a wait action and hand it back to the caller
+            return queue.ThenWaitFor(seconds);
+        }
+
+        public TaskQueue WaitUntil(UntilTaskPredicate condition) {
+            // find a cached queue or make a new one
+            TaskQueue queue = queueCache.Count > 0 ? queueCache.Pop() : new TaskQueue();
+
+            // store the queue in our active list
+            queues.Add(queue);
+
+            // queue up a conditional action and hand it back to the caller
+            return queue.ThenWaitUntil(condition);
+        }
+    }
+}
---
+++ Fruitless/TaskQueue.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fruitless {
+    public delegate bool UntilTaskPredicate(float elapsedTime);
+
+    public class TaskQueue : IAdvanceable<TimeSpan> {
+        // manage each step of the task queue
+        enum StepType {
+            Action,
+            TimeWait,
+            ConditionWait
+        }
+
+        class Step {
+            // type of step and timer for tracking how long we've been running the step
+            public StepType Type { get; set; }
+            public float Timer { get; set; }
+
+            // one of these three will matter depending on the step type
+            public Action Action { get; set; }
+            public float Length { get; set; }
+            public UntilTaskPredicate Condition { get; set; }
+        }
+
+        // cache of steps we can reuse to avoid garbage
+        static readonly Stack<Step> stepCache = new Stack<Step>();
+
+        // helper for getting a new step
+        static Step NewStep() {
+            Step step = stepCache.Count > 0 ?
+                stepCache.Pop() :
+                new Step();
+
+            step.Timer = 0f;
+
+            return step;
+        }
+
+        // steps for this queue
+        readonly Queue<Step> steps = new Queue<Step>();
+
+        // whether or not the queue is complete
+        public bool IsComplete {
+            get {
+                return steps.Count == 0;
+            }
+        }
+
+        public void Advance(TimeSpan delta) {
+            // if we're out of steps, we can't update anything
+            if (steps.Count == 0) {
+                return;
+            }
+
+            // see the next step and update its timer
+            Step s = steps.Peek();
+
+            s.Timer += (float)delta.TotalSeconds;
+
+            // handle the step
+            switch (s.Type) {
+                default:
+                    break;
+
+                // actions just get invoked
+                case StepType.Action: {
+                        s.Action();
+                        s.Action = null;
+                        stepCache.Push(steps.Dequeue());
+                    }
+                    break;
+
+                // timers check for a specific time before moving on
+                case StepType.TimeWait: {
+                        if (s.Timer >= s.Length) {
+                            stepCache.Push(steps.Dequeue());
+                        }
+                    }
+                    break;
+
+                // conditions require a delegate to return true to move on
+                case StepType.ConditionWait: {
+                        if (s.Condition(s.Timer)) {
+                            s.Condition = null;
+
+                            stepCache.Push(steps.Dequeue());
+                        }
+                    }
+                    break;
+            }
+        }
+
+        // adds an action to the queue
+        public TaskQueue Then(Action action) {
+            Step s = NewStep();
+
+            s.Type = StepType.Action;
+            s.Action = action;
+
+            steps.Enqueue(s);
+
+            return this;
+        }
+
+        // adds a timer to the queue
+        public TaskQueue ThenWaitFor(float seconds) {
+            Step s = NewStep();
+
+            s.Type = StepType.TimeWait;
+            s.Length = seconds;
+
+            steps.Enqueue(s);
+
+            return this;
+        }
+
+        // adds a conditional action to the queue
+        public TaskQueue ThenWaitUntil(UntilTaskPredicate condition) {
+            Step s = NewStep();
+
+            s.Type = StepType.ConditionWait;
+            s.Condition = condition;
+
+            steps.Enqueue(s);
+
+            return this;
+        }
+
+        // stops a task queue
+        public void Stop() {
+            while (steps.Count > 0) {
+                Step s = steps.Dequeue();
+
+                s.Action = null;
+                s.Condition = null;
+
+                stepCache.Push(s);
+            }
+        }
+    }
+}
