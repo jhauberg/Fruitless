7753227d9e6202365643602060d6a74878a7a11e
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2013-01-26 01:17:37 +0100

    New game: Squadtris.

---
+++ Games/Squadtris/Program.cs
@@ -0,0 +1,144 @@
+﻿using ComponentKit;
+using ComponentKit.Model;
+using Fruitless;
+using Fruitless.Components;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+using System;
+using System.Drawing;
+
+namespace Squadtris {
+    internal class Program : GameWindow {
+        internal static class Cursor {
+            public static int X {
+                get;
+                set;
+            }
+
+            public static int Y {
+                get;
+                set;
+            }
+        }
+
+        DefaultGameContext _context;
+
+        KeyboardState _ks;
+        KeyboardState _ksLast;
+
+        double _previousRenderTime;
+        double _averageRenderTime;
+
+        double _previousUpdateTime;
+        double _averageUpdateTime;
+
+        public Program()
+            : base(320, 480, GraphicsMode.Default, "Squadtris") {
+            WindowBorder = OpenTK.WindowBorder.Fixed;
+
+            CursorVisible = true;
+
+            VSync = VSyncMode.On;
+
+            TargetRenderFrequency = 60;
+            TargetUpdateFrequency = 60;
+
+            Cursor.X = Mouse.X;
+            Cursor.Y = Mouse.Y;
+        }
+
+        protected override void OnLoad(EventArgs e) {
+            base.OnLoad(e);
+
+            _context = new DefaultGameContext(
+                windowBoundsInPixels: ClientRectangle.Size);
+
+            _context.Registry.Entered += OnEntityEntered;
+            _context.Registry.Removed += OnEntityRemoved;
+
+            SpriteBatch spriteBatch = new SpriteBatch();
+            {
+                // add sprites
+            }
+
+            Entity.Create("tasks", new TaskManager());
+            Entity.Create("sprites", spriteBatch);
+        }
+
+        void OnEntityEntered(object sender, EntityEventArgs e) {
+
+        }
+
+        void OnEntityRemoved(object sender, EntityEventArgs e) {
+
+        }
+
+        protected override void OnResize(EventArgs e) {
+            base.OnResize(e);
+
+            _context.Bounds = ClientRectangle.Size;
+        }
+
+        bool KeyWasReleased(Key key) {
+            return _ksLast[key] && !_ks[key];
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e) {
+            base.OnUpdateFrame(e);
+
+            Cursor.X = Mouse.X;
+            Cursor.Y = Mouse.Y;
+
+            _ks = OpenTK.Input.Keyboard.GetState();
+
+            if (KeyWasReleased(Key.Escape)) {
+                Exit();
+            }
+
+            if (KeyWasReleased(Key.Tilde)) {
+                System.Diagnostics.Debug.WriteLine("~");
+            }
+
+            _context.Refresh(e.Time);
+
+            if (_context.IsOutOfSync) {
+                _context.Synchronize();
+            }
+
+            if (_previousUpdateTime > 0) {
+                double weightRatio = 0.1;
+
+                _averageUpdateTime = _averageUpdateTime * (1.0 - weightRatio) + _previousUpdateTime * weightRatio;
+            }
+
+            _previousUpdateTime = UpdateTime;
+
+            _ksLast = _ks;
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e) {
+            base.OnRenderFrame(e);
+
+            _context.Render();
+
+            SwapBuffers();
+
+            if (_previousRenderTime > 0) {
+                double weightRatio = 0.1;
+
+                _averageRenderTime = _averageRenderTime * (1.0 - weightRatio) + _previousRenderTime * weightRatio;
+            }
+
+            _previousRenderTime = RenderTime;
+        }
+
+        [STAThread]
+        internal static void Main() {
+            using (Program game = new Program()) {
+                game.Icon = new Icon("icon.ico");
+                game.Run(game.TargetUpdateFrequency);
+            }
+        }
+    }
+}
---
+++ Games/Squadtris/README.md
@@ -0,0 +1,9 @@
+# Squadtris
+
+![Concept mockup of the game.](https://raw.github.com/shrt/Fruitless/master/Games/Squadtris/mockup.png)
+
+It's like Tetris, except more horizontally focused and just way more fun.
+
+## Credits
+
+Thanks to [oryx](http://forums.tigsource.com/index.php?topic=8970.0) for most of the graphics!
